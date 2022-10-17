3207dce8a5a87fa49924fef74f3fe3c8aaa9d050
Алексей Печин <organner@mail.ru>
2022-10-17 19:49:09 +0300

    Initial commit

---
+++ src/main/java/com/example/conditional/profiles/DevProfile.java
@@ -0,0 +1,9 @@
+package com.example.conditional.profiles;
+
+public class DevProfile implements SystemProfile {
+
+    @Override
+    public String getProfile() {
+        return "Current profile is dev";
+    }
+}
